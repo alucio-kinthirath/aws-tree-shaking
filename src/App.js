fdfb4a4d1fb119e29ccb56bb818da83e4f92f026
Kenny Inthirath <kenny.inthirath@gmail.com>
2021-01-07 12:17:52 -0800

    import toggle

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import { Auth } from 'aws-amplify'
+// Comment out the import not being tested
+// import { API } from 'aws-amplify'
+import API from '@aws-amplify/api'
 
 function App() {
-  console.log({ Auth })
-  
+  console.log({ API })
+
   return (
     <div>lol</div>
   );
